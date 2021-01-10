ef62d4503a12a031947df16af2df34b85de9efad
Søren Bech Christensen <sbc@bangding.dk>
2021-01-10 14:26:50 +0100

    Changes suggested on StackOverflow

--- QrControl.xaml.cs
+++ QrControl.xaml.cs
@@ -18,35 +18,33 @@ namespace QrLabel
             InitializeComponent();
         }
 
-        private string _url;
         public string Url
         {
             get => (string)GetValue(UrlProperty);
-            set
-            {
-                if (value != _url)
-                {
-                    _url = value;
-                    QRCodeGenerator qrGenerator = new QRCodeGenerator();
-                    QRCodeData qrCodeData = qrGenerator.CreateQrCode(
-                        _url,
-                        QRCodeGenerator.ECCLevel.Q,
-                        true,
-                        true,
-                        QRCodeGenerator.EciMode.Utf8,
-                        6);
-                    XamlQRCode qrCode = new XamlQRCode(qrCodeData);
-                    DrawingImage qrCodeAsXaml = qrCode.GetGraphic(20);
-                    QrCodeImage.Source = qrCodeAsXaml;
-                }
-                SetValue(UrlProperty, value);
-            }
+            set => SetValue(UrlProperty, value);            
         }
 
         public static readonly DependencyProperty UrlProperty =
             DependencyProperty.Register(
             "Url", typeof(string),
-            typeof(QrControl)
-        );
+            typeof(QrControl),
+            new FrameworkPropertyMetadata(false,
+                FrameworkPropertyMetadataOptions.AffectsRender,
+                (o, e) => ((QrControl)o).OnIsUrlChanged()));
+
+        private void OnIsUrlChanged()
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(
+                Url,
+                QRCodeGenerator.ECCLevel.Q,
+                true,
+                true,
+                QRCodeGenerator.EciMode.Utf8,
+                6);
+            XamlQRCode qrCode = new XamlQRCode(qrCodeData);
+            DrawingImage qrCodeAsXaml = qrCode.GetGraphic(20);
+            QrCodeImage.Source = qrCodeAsXaml;
+        }
     }
 }
--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,13 +1,29 @@
-﻿namespace QrLabel.ViewModel
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace QrLabel.ViewModel
 {
-    class MainViewModel
+    class MainViewModel : INotifyPropertyChanged
     {
         private string _url;
 
         public string Url 
         { 
             get => _url;
-            set => _url = value;
+            set 
+            {
+                _url = value;
+                OnPropertyChanged();
+            }
+            
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
     }
 }
